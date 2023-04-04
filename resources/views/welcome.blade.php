e57d9e9c14ca7348ce217870495dde98dde3d5b5
Scott Knoll <scknoll88@gmail.com>
2023-04-03 21:59:51 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<Body>
+    <article>
+        <h1>My First Post</h1>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum consequatur totam nihil, perspiciatis natus ipsam praesentium dolores neque voluptatibus illum libero expedita veritatis fuga dolor itaque quasi vero nobis saepe.
+            </p>
+
+    </article>
+
+    <article>
+        <h1>My Second Post</h1>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum consequatur totam nihil, perspiciatis natus ipsam praesentium dolores neque voluptatibus illum libero expedita veritatis fuga dolor itaque quasi vero nobis saepe.
+            </p>
+
+    </article>
+</Body>
